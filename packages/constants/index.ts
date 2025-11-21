47f9169aaba41ae8cc1fb7739dbae9b72196219f
Treasure Uzoma <treasureuzoma650@gmail.com>
2025-11-21 01:38:34 +0100

    feat: update constants api base url to first check from .ev

--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -1,4 +1,7 @@
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL =
+  process.env.API_URL ||
+  process.env.NEXT_PUBLIC_API_URL ||
+  "http://localhost:5000";
 
 export const meta = {
   fullName: "Treasure Uzoma",
